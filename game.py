9a08540ae08e0475865798d240e9590beec529ae
Maciej Czaja <czaja.maciej.8@gmail.com>
2024-03-15 21:06:35 +0100

    Gotowa gra

--- game.py
+++ game.py
@@ -1,20 +1,81 @@
 from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def hello():
-    if request.method == "GET":
-        return """
-            <html>
-                <body>
-                    <form action="/" method="POST">
-                        
-                    </form>
-                </body>
-            </html>
-        """
-    else:
+HTML_START = """
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Guess The Number Game</title>
+        </head>
+        <body>
+            "Think about a number in range 0-1000 and I'll guess it in max. 10 tries!"<br>
+            "Press 'too small' if my number is smaller than yours,
+            'too big' if my number is bigger or 'you won' if I guessed it!"
+            
+            <form action="" method="POST">
+                <input type="hidden" name="min" value="{}"></input>
+                <input type="hidden" name="max" value="{}"></input>
+                <input type="submit" value="START!">
+            </form>
+        </body>
+    </html>
+"""
 
+HTML = """
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Guess The Number Game</title>
+        </head>
+        <body>
+            Your number is: {guess}
+            
+            <form action="" method="POST">
+                <input type="submit" name="user_answer" value="too big">
+                <input type="submit" name="user_answer" value="too small">
+                <input type="submit" name="user_answer" value="you won">
+                <input type="hidden" name="min" value="{min}">
+                <input type="hidden" name="max" value="{max}">
+                <input type="hidden" name="guess" value="{guess}">
+            </form>
+        </body>
+    </html>
+"""
+
+HTML_WIN = """
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Guess The Number Game</title>
+        </head>
+        <body>
+            "Yaaaaay! I won!"
+        </body>
+    </html>
+"""
+
+
+@app.route("/", methods=["GET", "POST"])
+def guess_the_number():
+    if request.method == "GET":
+        return HTML_START.format(0, 1000)
+    else:
+        min_number = int(request.form.get("min"))
+        max_number = int(request.form.get("max"))
+        user_answer = request.form.get("user_answer")
+        guess = int(request.form.get("guess", 500))
+        if user_answer == "too big":
+            max_number = guess
+        elif user_answer == "too small":
+            min_number = guess
+        elif user_answer == "you won":
+            return HTML_WIN.format(guess=guess)
+        guess = (max_number - min_number) // 2 + min_number
+        return HTML.format(guess=guess, min=min_number, max=max_number)
 
 
 if __name__ == "__main__":
